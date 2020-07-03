451e0b37d470ba15c2e2fb0a3e16122152b6416e
Resin Drake <resin@resindrake.net>
2020-07-03 12:51:00 +1000

    Initial commit

---
+++ servo-controller.ino
@@ -0,0 +1,14 @@
+// Pin definitions (Temporary exmaple values)
+const int Dout_servo_pin = 0;
+const int Ain_controller_pin = 1;
+const int Dout_USB_TX = 2;
+const int Din_USB_RX = 3;
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
